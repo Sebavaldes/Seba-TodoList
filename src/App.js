e44137d6459542fb4927d2408eb8fa1029b05db9
Sebastian Valdes <seba.valdes.es@gmail.com>
2023-12-18 17:59:46 -0300

    final todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [nuevoTodo, setNuevoTodo] = useState("");
+  const [todos, setTodos] = useState([])
+
+  const agregarTarea = () => {
+    if (nuevoTodo.trim() !== "") {
+      setTodos([...todos, nuevoTodo]);
+      setNuevoTodo("");
+    }
+  }
+
+  const deleteTodo = (indice) => {
+    const listaNueva = todos.filter((todo, i) => i !== indice)
+    setTodos(listaNueva);
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      agregarTarea();
+    }
+  }
+
+  const handleChange = (event) => {
+    setNuevoTodo(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center todo-container">
+      <h1 className='mt-5'>Tareas</h1>
+      <div className="input-group mb-3 todo-input">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Agregar tarea"
+          value={nuevoTodo}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+        />
+      </div>
+      <ul className="list-group">
+        {todos.map((todo, indice) => (
+          <li key={indice} className="list-group-item d-flex justify-content-between align-items-center">
+            {todo}
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={() => deleteTodo(indice)}
+            >
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
